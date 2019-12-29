e1f5db11aa89074226d41f19efc576235261d2fd
1cekrim <1cekrim.dev@gmail.com>
2019-12-29 14:58:15 +0900

    feat: Add http status codes and Implement check_game_started

--- Sources/connect5-server.py
+++ Sources/connect5-server.py
@@ -61,9 +61,9 @@ def check_login(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if not 'player_name' in session:
-            return 'Please login first'
+            return 'Please login first', 401
         if not session['player_name'] in current_app.players:
-            return 'Session has expired.'
+            return 'Session has expired.', 401
         return func(*args, **kwargs)
     return wrapper
 
@@ -72,24 +72,33 @@ def check_admin(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if not 'admin_password' in request.form:
-            return 'Access denied.'
+            return 'Bad Request.', 400
         if request.form['admin_password'] != ADMIN_PASSWORD:
-            return 'Access denied.'
+            return 'Access denied.', 403
         return func(*args, **kwargs)
     return wrapper
+
+
+def check_game_started(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_app.players[session['player_name']]:
+            return 'Game did not start.', 409
+        return func(*args, **kwargs)
+    return wrapper 
 
 
 @app.route('/login', methods=['POST'])
 def login():
     if 'player_name' in session:
-        return f'Please logout first. {escape(session["player_name"])}'
+        return f'Please logout first. {escape(session["player_name"])}', 409
     player_name = request.form['player_name']
     if player_name in current_app.players:
-        return f'{player_name} is in conflict.'
+        return f'{player_name} is in conflict.', 409
 
     session['player_name'] = player_name
     current_app.players[player_name] = Player(player_name)
-    return f'Hello. {player_name}'
+    return f'Hello. {player_name}', 200
 
 
 @app.route('/logout', methods=['POST'])
@@ -98,9 +107,9 @@ def logout():
     player_name = session['player_name']
     session.pop('player_name', None)
     if not player_name in current_app.players:
-        return 'You are already logged out.'
+        return 'You are already logged out.', 409
     del current_app.players[player_name]
-    return f'Good bye. {player_name}'
+    return f'Good bye. {player_name}', 200
 
 
 @app.route('/remove_player', methods=['POST'])
@@ -108,21 +117,21 @@ def logout():
 def remove_player():
     player_name = request.form['player_name']
     if not player_name in current_app.players:
-        return f'{player_name} does not exist.'
+        return f'{player_name} does not exist.', 409
     del current_app.players[player_name]
-    return 'Success.'
+    return 'Success.', 200
 
 
 @app.route('/player', methods=['GET'])
 def player():
-    return jsonify(list(current_app.players.keys()))
+    return jsonify(list(current_app.players.keys())), 200
 
 
 @app.route('/make_match', methods=['POST'])
 @check_admin
 def make_match():
     if not ('black' in request.form and 'white' in request.form and request.form['black'] in current_app.players and request.form['white'] in current_app.players):
-        return 'Invalid player.'
+        return 'Invalid player.', 400
     config = {}
     config['black'] = request.form['black']
     config['white'] = request.form['white']
@@ -141,9 +150,9 @@ def state():
     try:
         result = player.game.get_state(player.player_name)
     except InvalidTurnException:
-        return None
+        return 'Not your turn.', 403
     else:
-        return jsonify(result)
+        return jsonify(result), 200
 
 
 def init_app():
