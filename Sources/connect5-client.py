0246989bba714985a57bb15fdffadfb9b83387cc
1cekrim <1cekrim.dev@gmail.com>
2019-12-29 14:30:44 +0900

    feat: Implement make_session, session, login, and logout

---
+++ Sources/connect5-client.py
@@ -0,0 +1,59 @@
+import requests
+from functools import wraps
+
+host = 'http://127.0.0.1:12345/'
+ADMIN_PASSWORD = 'rla92233'
+
+command_functions = {}
+
+def command_func(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print('\n[Command Start]')
+        func(*args, **kwargs)
+        print('[Command End]\n')
+    command_functions[func.__name__] = wrapper
+    return wrapper
+
+sessions = {}
+@command_func
+def make_session(command):
+    if len(command) != 1:
+        print('session [session name]')
+    elif command[0] in sessions:
+        print('[Error] Already exist.')
+    else:
+        sessions[command[0]] = requests.Session()
+        print('[Result] Success.')
+
+@command_func
+def session(command):
+    print(*list(sessions.keys()), sep='\n')
+
+@command_func
+def login(command):
+    if len(command) != 2:
+        print('login [session name] [player name]')
+    elif command[0] not in sessions:
+        print('[Error] Invalid session.')
+    else:
+        print('[Result] ' + sessions[command[0]].post(host + 'login', {'player_name':command[1]}).text)
+
+@command_func
+def logout(command):
+    if len(command) != 1:
+        print('logout [session name]')
+    elif command[0] not in sessions:
+        print('[Error] Invalid session.')
+    else:
+        print('[Result] ' + sessions[command[0]].post(host + 'logout').text)
+
+while True:
+    command = str(input())
+    if command == 'exit':
+        print('exit.')
+        break
+    if command.split(' ')[0] not in command_functions:
+        print('Invalid command.')
+        continue
+    command_functions[command.split(' ')[0]](command.split(' ')[1:])
